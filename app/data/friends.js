fd63054882b468a0884dd7a47fe95eced9324160
Billy Hodes <billyhodes@gmail.com>
2018-11-09 19:25:49 -0600

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendList = [
+    {
+        name: 'Robert DiNero',
+        profilePic: 'https://i.pinimg.com/originals/84/39/96/843996971aa1ea96d0c1c69395b5ee13.jpg',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: 'Marlon Brando',
+        profilePic: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/62427-paramount.jpg?itok=N-mA96mQ&resize=1100x1100',
+        scores: [
+            "3",
+            "2",
+            "4",
+            "3",
+            "3",
+            "3",
+            "5",
+            "5",
+            "3",
+            "1"
+        ]
+    }
+]
+
+module.exports = friendList;
